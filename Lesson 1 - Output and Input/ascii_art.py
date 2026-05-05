25e8cf380e6ec26dd6250573f3e0c65ece0f1e05
HarryShanks <shanksh@wsc.school.nz>
2026-05-05 15:04:37 +1200

    Made Askii Art
    Made my first ascii art plus more complex arts

---
+++ Lesson 1 - Output and Input/ascii_art.py
@@ -0,0 +1,33 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+print("First Ascii Art")
+print("   /\\")
+print("  /oo\\")
+print("   y  ")
+print("\______/")
+
+print("Teacher's Ascii Art")
+print("  ----------")
+print(" /          \\ \\")
+print("|    O    O    |")
+print("|       V      |")
+print(" \\ \\___________/")
+
+print("Gun")
+print("    ---------------")
+print("   /     ----------")
+print("  /     /")
+print(" /     /")
+print("/_____/")
+
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
+print("Mammoth")
+print ("  -------")
+print (" /       \\")
+print("/   O O   \\")
+print ("/          \\")
+print("--------------")
---
+++ main.py
@@ -0,0 +1,3 @@
+print("I'm coding")
+print("Poppy Playtime is awesome!!")
+print("Whooohohohohhoho")
