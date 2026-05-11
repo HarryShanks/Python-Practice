953758986e325daf35c79c959dedf2fe15086eb5
HarryShanks <shanksh@wsc.school.nz>
2026-05-11 12:03:56 +1200

    Turning Days into Seconds
    Turning Days into Seconds with Python. (Not currently working)

---
+++ Lesson 2 - Data Types and Variables/seconds_calculator.py
@@ -0,0 +1,39 @@
+# Create a calculator that asks the user for a number (of days)
+# and outputs how many seconds in that number of days
+
+# Values - start by writing constants to hold:
+# The number of seconds in a minute
+NUMBER_OF_SECONDS_IN_A_MINUTE = 60
+# The number of minutes in an hour
+NUMBER_OF_MINUTES_IN_A_HOUR = 60
+# The number of hours in a day
+NUMBER_OF_HOURS_IN_A_DAY = 24
+
+# Get input from the user and save it in a variable
+print("Give me a number of days and I will tell you how many seconds are in that day")
+input("Give me a number of days.")
+print ()
+
+# Change the value into an integer and resave in the variable
+
+
+# Calculate the number of seconds using * with the input and your constants. 
+# Save it in a new variable.
+
+# Output the answer
+
+# ---------------------------------
+
+# EXTENSION 1
+# Also output how many total hours and how many total minutes in the days
+
+# ---------------------------------
+# EXTENSION 2
+# Create another calculator that does the opposite (input is seconds, output is days)
+
+# ---------------------------------
+
+# EXPERT (for those who already know some Python)
+# Create the calculator above, but...
+#   allow your user to choose the input and output type (seconds, minutes, hours, days)
+#   Loop the calculator so they can do it again with having to reopen the program.
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-name = 'Harry' 
+#name = 'Harry' 
 #print ("My name is" + name)
 #print ("My name is",name)
-age = "15"
+#age = "15"
 #print(name + "is" + age)
 #print(name, "is", age)
 
@@ -11,5 +11,11 @@ age = "15"
 #print("Hello")
 
 #input("Name?\n")
-print("Name?")
-input()
+#print("Name?")
+#input()
+#age = 5
+#print(" He is" + age) X
+#print ("He is " + str (age)) Correct
+age = input ("What's you Age?")
+#print("Birth year:", 2026 - age) X
+print("Birth year:", 2026 - int (age)) 
