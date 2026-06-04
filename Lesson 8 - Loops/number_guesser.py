732abdfb9ada694c5f73d302a27d6d132ceb8245
HarryShanks <shanksh@wsc.school.nz>
2026-06-05 09:05:56 +1200

    Number Guesser
    From 1 - 100

--- Lesson 8 - Loops/number_guesser.py
+++ Lesson 8 - Loops/number_guesser.py
@@ -2,13 +2,13 @@
 # IMPORTS
 import random
 # VARIABLES
-secret_number = random.randint (1, 1000)
+secret_number = random.randint (1, 100)
 guesses = 0
 current_guess = 0
 
 print ("Welcome to the Higher or Lower Number Guessing Game")
 # START THE GAME
-current_guess = int(input("Guess a number from 1 to 1000\n"))
+current_guess = int(input("Guess a number from 1 to 100\n"))
 print ("Checking guess...")
 guesses +=1
 while current_guess != (secret_number):
@@ -16,16 +16,16 @@ while current_guess != (secret_number):
         print ()
         print ("Not Correct\nToo High")
         guesses +=1
-        current_guess = int(input("Guess a number from 1 to 1000\n"))
+        current_guess = int(input("Guess a number from 1 to 100\n"))
         print("Checking guess...")
     elif secret_number > current_guess:
         print ()
         print ("Not Correct\nToo Low")
         guesses +=1
-        current_guess = int(input("Guess a number from 1 to 1000\n"))
+        current_guess = int(input("Guess a number from 1 to 100\n"))
         print("Checking guess")
     else: 
-        input ("I said guess a number from 1 to 1000\n") 
+        input ("I said guess a number from 1 to 100\n") 
         print ("Checking Guess")
 if  current_guess == (secret_number):
     print ()
