befad4d5fcb506c62791084a15963b55529ef651
HarryShanks <shanksh@wsc.school.nz>
2026-06-15 12:05:02 +1200

    Start Wordle

--- main.py
+++ main.py
@@ -4,15 +4,17 @@
 #print ("1")
 #time.sleep (1)
 #print ("2")
+foods = ['apples','carrots','muesli']
+for i in range(len(foods)):
+    print(f'{i+1}.{foods[i]}')
 
-shopping_list = ['apples', 'oranges','melons']
-print(shopping_list)
-shopping_list.append('carrots')
-shopping_list.remove('oranges')
-print(shopping_list)
-shopping_list.pop(0)
-shopping_list.insert(1,'apples')
-print(shopping_list)
-shopping_list.sort()
-print(shopping_list)
+
+
+
+
+
+
+
+
+
 
