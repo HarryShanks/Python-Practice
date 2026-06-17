127e8670ac7de01531acc7646e5006e0133e4e79
HarryShanks <shanksh@wsc.school.nz>
2026-06-18 10:50:24 +1200

    Part of wordle

--- Lesson 11 - For/wordle.py
+++ Lesson 11 - For/wordle.py
@@ -20,50 +20,74 @@ print ("Guess a five letter word and wordle will decide whether the letter is in
 while play == True:
 
     # TODO Create word variable and store a random word from your list (using random.choice)
-    secret_word = random.choice
+    secret_word = random.choice(words).lower().strip()
     # USER INPUT
     
     guess = input("What is your first guess?").lower().strip()
     # TODO Get user's first guess and save it into a variable
-    while guess != len(5):
+    while len(guess)!= 5:
         print()
         print("The guess needs to be 5 letters")
         print ("Try again")
-        print ("What is your first guess?")
+        guess = input ("What is your first guess?").lower().strip()
     # TODO Create a while loop if the guess is not 5 characters long
         # TODO Tell them it's not 5 letters and to try again
-    while guess == len(5):
+    while len(guess) == 5:
     # TODO Check if they got it correct and if they did, tell them so and then break the loop
         if guess == secret_word:
             print("You got it correct!")
             break
     # TODO Create a for loop that loops 5 times
-    for i in range(5):
-        input("What is your next guess?").lower().strip()
-        while guess != len(5):
-            print()
-            print("The guess needs to be 5 letters")
-            print ("Try again")
-            print ("What is your next guess?")
-        while guess == len(5):
-             if guess == secret_word:
+        else:
+            for i in range(5):
+                if guess[i] == secret_word[i]:
+                    print(f"{guess[i]} is in the right spot")
+                    continue
+                        # TODO Otherwise check if the current letter of user_input is in the word and if it is, tell them that letter is in the wrong position
+                elif guess[i] in secret_word:
+                    print(f"{guess[i]} is in the word but is in the wrong spot")
+                    continue
+                        # TODO Else tell them that letter is wrong
+                else:
+                    print (f"{guess[i]} is not in the word ")
+                    continue
+            input("What is your next guess?").lower().strip()
+            while len(guess)!= 5:
                 print()
-                print("You got it correct!")
-                break
+                print("The guess needs to be 5 letters")
+                print ("Try again")
+                print ("What is your next guess?")
+            while len(guess)== 5:
+                if guess == secret_word:
+                    print()
+                    print("You got it correct!")
+                    break
 
         # TODO Check if the current letter of user_input (user_input[i]) is the same as the i letter of the word and if it is tell them they got that letter correct
-        for j in range(5):
-            if guess[j] == secret_word[j]:
-                print()
-                print(f"{guess[j]} is in the right spot")
-        # TODO Otherwise check if the current letter of user_input is in the word and if it is, tell them that letter is in the wrong position
-            elif guess[j] in secret_word:
-                print()
-                print(f"{guess[j]} is in the word but is in the wrong spot")
-        # TODO Else tell them that letter is wrong
-            else:
-                print()
-                print (f"{guess[j]} is not in the word ")
+                else:
+                    for j in range(5):
+                        if guess[j] == secret_word[j]:
+                            print(f"{guess[j]} is in the right spot")
+                            continue
+                        # TODO Otherwise check if the current letter of user_input is in the word and if it is, tell them that letter is in the wrong position
+                        elif guess[j] in secret_word:
+                            print(f"{guess[j]} is in the word but is in the wrong spot")
+                            continue
+                        # TODO Else tell them that letter is wrong
+                        else:
+                            print (f"{guess[j]} is not in the word ")
+                            continue
+                    input("What is your next guess?").lower().strip()
+                    while len(guess)!= 5:
+                        print()
+                        print("The guess needs to be 5 letters")
+                        print ("Try again")
+                        print ("What is your next guess?")
+                    while len(guess)== 5:
+                        if guess == secret_word:
+                            print()
+                            print("You got it correct!")
+                            break
 # TODO Ask if they want to play again. If they don't, set play to false.
 again = input("Do you want to play again?").lower().strip()
 if again in "yes":
---
+++ Lesson 13 - Functions/menu_13.py
@@ -0,0 +1,54 @@
+"""
+PROGRAM: Menu
+This starts with a menu so users can run 1 of 3 different programs:
+1.
+2.
+3.
+"""
+
+#===============================
+# IMPORTS
+#===============================
+
+# TODO Once you've copied your 3 programs below, move any imports to here
+
+#===============================
+# FUNCTIONS
+#===============================
+
+# TODO Create a function to hold your first program. You can call it run_program_name(). 
+    # TODO Copy and paste the programs code inside (meaning indented)
+
+
+# TODO Create a function to hold your second program. You can call it run_program_name(). 
+    # TODO Copy and paste the programs code inside (meaning indented)
+
+
+# TODO Create a function to hold your third program. You can call it run_program_name(). 
+    # TODO Copy and paste the programs code inside (meaning indented)
+
+
+# TODO Create a main function
+
+    # TODO Introduce your menu, including outputting all three options with numbers
+    
+    # TODO Ask user which program they'd like to run and store input in a variable
+
+    # TODO Check if they said "1" (with speech marks)
+
+        # TODO Call program 1 function
+
+    # TODO Otherwise check if they said "2" (with speech marks)
+
+        # TODO Call program 2 function
+
+    # TODO Otherwise check if they said "3" (with speech marks)
+
+        # TODO Call program 3 function
+
+
+#===============================
+# EXECUTION
+#===============================
+
+# TODO Call main function
